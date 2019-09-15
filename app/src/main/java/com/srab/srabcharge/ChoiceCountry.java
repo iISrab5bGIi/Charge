dfc4f76b062696caa62c41272cb327bf1fb312ce
iISrab5bGIi <55337026+iISrab5bGIi@users.noreply.github.com>
2019-09-15 07:38:34 +0300

    Spinner edit

--- app/src/main/java/com/srab/srabcharge/ChoiceCountry.java
+++ app/src/main/java/com/srab/srabcharge/ChoiceCountry.java
@@ -2,6 +2,7 @@ package com.srab.srabcharge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -20,7 +21,7 @@ public class ChoiceCountry extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice_country);
 
-        ImageView next = findViewById(R.id.imgv);
+        final ImageView next = findViewById(R.id.imgv);
         final Spinner SC = (Spinner) findViewById(R.id.sp);
         final List<String> Scl = new ArrayList<String>();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,Scl);
@@ -33,6 +34,27 @@ public class ChoiceCountry extends AppCompatActivity {
         SC.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if (i==0){
+
+                    next.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent n1 = new Intent(ChoiceCountry.this,Ch_Sporter_Egypt.class);
+                            startActivity(n1);
+                        }
+                    });
+                    if (i==1){
+                        next.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent n2 = new Intent(ChoiceCountry.this,Ch_Sporter_Egypt.class);
+                                startActivity(n2);
+                            }
+                        });
+                    }
+                }
+
 
 
             }
